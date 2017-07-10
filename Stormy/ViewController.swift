d33795ecd7b3683ecf9dd58b6d1b2cea242fb2d4
Sarah Kohl <sarahcharlottekohl@gmail.com>
2017-07-09 20:47:36 -0700

    Initial Commit

---
+++ Stormy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stormy
+//
+//  Created by yuzu on 2017/07/09.
+//  Copyright © 2017 Sarah Kohl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
